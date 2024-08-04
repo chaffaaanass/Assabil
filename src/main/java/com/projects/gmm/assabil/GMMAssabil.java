48feaabd7655e418598625f96dd489a41bdf8f83
anass <chaffaaanass@gmail.com>
2024-08-04 19:32:45 +0100

    Initial Commit (Dependencies)

---
+++ src/main/java/com/projects/gmm/assabil/GMMAssabil.java
@@ -0,0 +1,11 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+package com.projects.gmm.assabil;
+
+public class GMMAssabil {
+
+    public static void main(String[] args) {
+        
+    }
+}
